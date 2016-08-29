5f9405da63495c4e5ea4e1adb1df0c721dc93fa1
Paul Biggar <paul.biggar@gmail.com>
2016-08-29 18:12:14 -0400

    2 more steps through the process

--- app/Main.hs
+++ app/Main.hs
@@ -15,9 +15,13 @@ import           Data.ByteString (ByteString, isInfixOf)
 import qualified Data.Maybe as Maybe
 import qualified Text.XML.HaXml.ParseLazy as XMLParse
 import qualified Text.XML.HaXml.Types as XML
+import qualified Network.HTTP.Types.Header (HeaderName)
+import qualified System.Console.ANSI as ANSI
 
 main :: IO ()
-main = Warp.run 8080 app
+main = do
+  putStrLn "starting"
+  Warp.run 8080 app
 
 app :: Wai.Application
 app req respond = bracket_
@@ -30,28 +34,60 @@ cleaning = return ()
 
 responding :: (Wai.Response -> IO a) -> Wai.Request -> IO a
 responding responder req = do
+  ANSI.setSGR [ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.Red]
+  putStrLn "\n\n"
+  putStrLn "Request: "
+  putStrLn $ show req
+  ANSI.setSGR [ANSI.Reset]
+
   let method = C8BS.unpack $ Wai.requestMethod req
   let path = C8BS.unpack $ Wai.rawPathInfo req
   body <- (Wai.requestBody req)
-  putStrLn "Starting: "
-  putStrLn $ show req
+
+  ANSI.setSGR [ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.Yellow]
   putStrLn method
   putStrLn path
   putStrLn $ show body
 
-  (code, response) <- respond method path body
-  putStrLn $ show code
+  (code, headers, content) <- respond method path body
 
-  responder $ Wai.responseLBS code [] (C8LBS.pack response)
+  let response = Wai.responseLBS code headers (C8LBS.pack content)
+
+  ANSI.setSGR [ANSI.SetColor ANSI.Foreground ANSI.Vivid ANSI.Red]
+  putStrLn "\n\nResponse: "
+  putStrLn $ show code
+  putStrLn $ show headers
+  putStrLn $ show content
+  ANSI.setSGR [ANSI.Reset]
+
+
+  responder response
+
 
 type BS = DBS.ByteString
 type LBS = C8LBS.ByteString
 
-respond :: String -> String -> BS -> IO (Status.Status, String)
+s404 = (Status.status404, [], "")
+c200 r = (Status.status200, [], response0)
+h200 hs = (Status.status200, hs, "")
+
+respond :: String -> String -> BS -> IO (Status.Status, [(Network.HTTP.Types.Header.HeaderName, ByteString)], String)
 respond "PROPFIND" "/" body
- | isInfixOf "current-user-principal" body = return (status200, response0)
- | otherwise = return (Status.status404, "")
-respond _ _ _ = return (Status.status404, "")
+ | isInfixOf "current-user-principal" body =
+   return $ c200 response0
+ | otherwise =
+   return s404
+respond "OPTIONS" "/principals/users/cdaboo/" _ =
+   return $ h200 response1Headers
+respond "PROPFIND" "/principals/users/cdaboo" body
+ | isInfixOf "addressbook-home-set" body =
+   return $ c200 response2
+ | otherwise =
+   return s404
+
+respond _ _ _ = return s404
+
+
 
 response0 = unlines [
    "<d:multistatus xmlns:d=\"DAV:\">"
@@ -60,11 +96,27 @@ response0 = unlines [
  , "      <d:propstat>"
  , "          <d:prop>"
  , "              <d:current-user-principal>"
- , "                  <d:href>/principals/users/spamdav/</d:href>"
+ , "                  <d:href>/principals/users/cdaboo/</d:href>"
  , "              </d:current-user-principal>"
  , "          </d:prop>"
  , "          <d:status>HTTP/1.1 200 OK</d:status>"
  , "      </d:propstat>"
  , "  </d:response>"
  , "</d:multistatus>"
+ ]
+
+response1Headers :: [(Network.HTTP.Types.Header.HeaderName, ByteString)]
+response1Headers = [
+   ("Allow", "OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, COPY, MOVE")
+ , ("Allow", "MKCOL, PROPFIND, PROPPATCH, LOCK, UNLOCK, REPORT, ACL")
+ , ("DAV", "1, 2, 3, access-control, addressbook")
+ , ("DAV", "extended-mkcol")
+ ]
+
+
+response2 = unlines [
+   "<C:addressbook-home-set xmlns:D=\"DAV:\""
+ , "        xmlns:C=\"urn:ietf:params:xml:ns:carddav\">"
+ , "       <D:href>/bernard/addresses/</D:href>"
+ , "     </C:addressbook-home-set>"
  ]
